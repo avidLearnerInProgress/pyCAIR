6389e993fd0265c991a0d24d9cf715d064bc14f3
quanturtle <alvaromachucar@gmail.com>
2023-12-12 09:32:01 -0300

    added documentation to cropByColumn, updated requirements, updated deprecated code, modified setup.py to install dependencies

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 from setuptools import setup
-
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open(path.join(this_directory, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
 python_requires = '>3.4'
-    
+
 setup(
-  name = 'pyCAIR',
-  packages = ['pyCAIR'],
-  version = '0.1.13',
-  description = ' This module provides a simple yet powerful mechanism to resize images using Seam Carving Algorithm.',
-  long_description = long_description,
-  long_description_content_type='text/markdown',
-  author = 'Chirag Shah',
-  author_email = 'chiragshah9696@gmail.com',
-  url = 'https://github.com/avidLearnerInProgress/pyCAIR',
-  download_url = 'https://github.com/avidLearnerInProgress/pyCAIR/archive/0.1.tar.gz',
-  license = 'GPL-3.0',
-  keywords = ['python', 'openCV', 'image-processing', 'dynamic-programming', 'seam-carving'],
-  classifiers = [],
+    name='pyCAIR',
+    packages=['pyCAIR'],
+    version='0.1.13',
+    description='This module provides a simple yet powerful mechanism to resize images using Seam Carving Algorithm.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Chirag Shah',
+    author_email='chiragshah9696@gmail.com',
+    url='https://github.com/avidLearnerInProgress/pyCAIR',
+    download_url='https://github.com/avidLearnerInProgress/pyCAIR/archive/0.1.tar.gz',
+    license='GPL-3.0',
+    keywords=['python', 'openCV', 'image-processing', 'dynamic-programming', 'seam-carving'],
+    classifiers=[],
+    install_requires=requirements,
+    python_requires=python_requires
 )
